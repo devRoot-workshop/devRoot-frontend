9e948185bd70878decd1af5e91ce2a242475c292
Milan <fulopmilan170@gmail.com>
2024-11-10 14:39:47 +0100

    init: shadcn-ui

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <div>
-      <p>test</p>
+      <p>Text</p>
+      <Button variant="default">Button</Button>
     </div>
   );
 }
