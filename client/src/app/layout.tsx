695f7903aa7769ae62ad86548c800fe748bfbadc
Milan <fulopmilan170@gmail.com>
2024-12-06 17:49:24 +0100

    feat: hero section

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../lib/authContext";
 
@@ -12,6 +13,12 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -27,11 +34,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased dark`}
       >
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,24 @@
+import Button from '@/components/button/Button';
+import styles from './page.module.css'
 import Header from "@/components/header/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className={styles["hero-section"]}>
+      </div>
+
+      <div className={styles["text-wrapper"]}>
+        <h1 className="title">Közösen írjuk a jövőd kódját</h1>
+        <h3 className="my-4 title-description">
+          Egy közösség, ahol tanárok és diákok programozási feladatokat oszthatnak meg és használhatnak.
+        </h3>
+        <div className={'my-7 ' + styles["hero-button-container"]}>
+          <Button>Feladatok</Button>
+          <Button ghost={true}>Regisztráció</Button>
+        </div>
+      </div>
     </div>
   );
 }
