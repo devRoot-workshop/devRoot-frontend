942f7a0876951a49c487683f958f533bcfdc640b
Milan <fulopmilan170@gmail.com>
2025-02-09 18:32:35 +0100

    feat: header for upload container

--- src/app/feladatok/feltoltes/page.tsx
+++ src/app/feladatok/feltoltes/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import React, { useState } from "react";
 import axios from "axios";
 import { useAuth } from "@/lib/authContext";
@@ -11,6 +10,19 @@ import TagComponent from "@/components/boxes/tag/TagComponent";
 import AddTag from "@/components/boxes/tag/AddTag";
 import DropDown from "@/components/dropdown/DropDown";
 import { domain, port, secure } from "@/lib/global/global";
+import ExampleCodeContainer from "./exampleCodeBox/ExampleCodeBox";
+
+interface QuestType {
+    id: number;
+    title: string;
+    taskDescription: string;
+    exampleCodes: ExampleCodeType[];
+    console: string;
+    difficulty: number;
+    availableLanguages: string[];
+    created: string;
+    tags: TagType[];
+}
 
 const UploadPage: React.FC = () => {
     const { user } = useAuth();
@@ -18,15 +30,16 @@ const UploadPage: React.FC = () => {
         id: 0,
         title: "",
         taskDescription: "",
-        difficulty: 0,
-        created: new Date().toISOString(), 
-        code: "",
+        exampleCodes: [],
         console: "",
-        language: 0,
+        difficulty: 0,
+        availableLanguages: [],
+        created: new Date().toISOString(),
+        tags: []
     });
     const [tags, setTags] = useState<TagType[]>([]);
-
     const [submissionSuccess, setSubmissionSuccess] = useState(false);
+    const [activeTab, setActiveTab] = useState("Feladat");
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
@@ -45,19 +58,21 @@ const UploadPage: React.FC = () => {
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
-        const convertedFormData = {
-            difficulty: formData.difficulty,
-            title: formData.title,
-            taskDescription: formData.taskDescription,
-            created: formData.created,
-            tagId: tags.map((tag) => tag.id),
+        const questData: QuestType = {
+            id: formData.id ?? 0,
+            title: formData.title ?? "",
+            taskDescription: formData.taskDescription ?? "",
+            exampleCodes: formData.exampleCodes ?? [],
+            console: formData.console ?? "",
+            difficulty: formData.difficulty ?? 0,
+            availableLanguages: formData.availableLanguages ?? [],
+            created: formData.created ?? new Date().toISOString(),
+            tags: tags
         };
-
         try {
             const response = await axios.post(
-                `http${secure ? 's' : ''}://${domain}:${port}/Quest/CreateQuest`,
-                convertedFormData,
+                `http${secure ? "s" : ""}://${domain}:${port}/Quest/CreateQuest`,
+                questData,
                 {
                     headers: {
                         Authorization: `Bearer ${await user?.getIdToken()}`,
@@ -77,12 +92,12 @@ const UploadPage: React.FC = () => {
             id: 0,
             title: "",
             taskDescription: "",
-            difficulty: 0,
-            created: new Date().toISOString(), 
-            code: "",
+            exampleCodes: [],
             console: "",
-            language: 0,
-            tags: [],
+            difficulty: 0,
+            availableLanguages: [],
+            created: new Date().toISOString(),
+            tags: []
         });
         setSubmissionSuccess(false);
     };
@@ -90,7 +105,21 @@ const UploadPage: React.FC = () => {
     if (submissionSuccess) {
         return (
             <div>
-                <div className={styles.container}>
+                <div className={styles.tabHeader}>
+                    <button
+                        className={activeTab === 'Feladat' ? styles.activeTab : styles.tab}
+                        onClick={() => setActiveTab('Feladat')}
+                    >
+                        Feladat
+                    </button>
+                    <button
+                        className={activeTab === 'Megoldások' ? styles.activeTab : styles.tab}
+                        onClick={() => setActiveTab('Megoldások')}
+                    >
+                        Megoldások
+                    </button>
+                </div>
+                <div className={styles.formContainer}>
                     <h1>Feltöltés sikeres!</h1>
                     <Button type="button" size="large" onClick={newUpload}>
                         Új feltöltés
@@ -101,57 +130,80 @@ const UploadPage: React.FC = () => {
     }
 
     return (
-        <div>
-            <form className={styles.container} onSubmit={handleSubmit}>
-                <h1 className={styles.pageTitle}>Feladat feltöltése</h1>
-                <InputBox
-                    name="title"
-                    onChange={handleChange}
-                    value={formData.title!}
-                    placeholderText="Title"
-                />
-                <InputBox
-                    type="textarea"
-                    name="taskDescription"
-                    onChange={handleChange}
-                    value={formData.taskDescription!}
-                    placeholderText="Description"
-                />
-                <div className={styles.row}>
-                    <DropDown
-                        name="difficulty"
-                        value={formData.difficulty!.toString()}    
-                        onChange={(e) => handleDifficultyChange(Number(e.target.value))}
-                        options={[
-                            { value: "0", display: "Könnyű" },
-                            { value: "1", display: "Közepes" },
-                            { value: "2", display: "Nehéz" },
-                        ]}
+        <div className={styles.container}>
+            <div className={styles.tabHeader}>
+                <button
+                    className={activeTab === 'Feladat' ? styles.activeTab : styles.tab}
+                    onClick={() => setActiveTab('Feladat')}
+                >
+                    Feladat
+                </button>
+                <button
+                    className={activeTab === 'Megoldások' ? styles.activeTab : styles.tab}
+                    onClick={() => setActiveTab('Megoldások')}
+                >
+                    Megoldások
+                </button>
+            </div>
+            {activeTab === 'Feladat' && (
+                <form className={styles.formContainer} onSubmit={handleSubmit}>
+                    <h1 className={styles.pageTitle}>Általános adatok</h1>
+                    <InputBox
+                        name="title"
+                        onChange={handleChange}
+                        value={formData.title!}
+                        placeholderText="Cím"
                     />
-                    <TagContainer>
-                        {tags.map((tag) => (
-                            <TagComponent
-                                key={tag.id}
-                                id={tag.id}
-                                text={tag.name}
-                                setTags={setTags}
-                            />
-                        ))}
-                        <AddTag setTags={setTags} tags={tags} />
-                    </TagContainer>
+                    <InputBox
+                        type="textarea"
+                        name="taskDescription"
+                        onChange={handleChange}
+                        value={formData.taskDescription!}
+                        placeholderText="Leírás"
+                    />
+                    <div className={styles.row}>
+                        <DropDown
+                            name="difficulty"
+                            value={formData.difficulty!.toString()}
+                            onChange={(e) => handleDifficultyChange(Number(e.target.value))}
+                            options={[
+                                { value: "0", display: "Könnyű" },
+                                { value: "1", display: "Közepes" },
+                                { value: "2", display: "Nehéz" },
+                            ]}
+                        />
+                        <TagContainer>
+                            {tags.map((tag) => (
+                                <TagComponent
+                                    key={tag.id}
+                                    id={tag.id}
+                                    text={tag.name}
+                                    setTags={setTags}
+                                />
+                            ))}
+                            <AddTag setTags={setTags} tags={tags} />
+                        </TagContainer>
+                    </div>
+                </form>
+            )}
+            {activeTab === 'Megoldások' && (
+                <div className={styles.formContainer}>
+                    <h1 className={styles.pageTitle}>Megoldások</h1>
+                    a
                 </div>
-                <Button type="button" size="small" color="pale">
-                    Fájl hozzáadása
-                </Button>
-
-                <div className={styles.buttonGroup}>
-                    <Button type="submit" size="large">
-                        Feltöltés
-                    </Button>
-                </div>
-            </form>
+            )}
         </div>
     );
 };
 
 export default UploadPage;
+
+/*
+
+                    <div className={styles.buttonGroup}>
+                        <Button type="submit" size="large">
+                            Feltöltés
+                        </Button>
+                    </div>
+
+*/
