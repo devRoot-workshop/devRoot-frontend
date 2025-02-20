bf241db11809f0ab2b43606c874cdd26dfcde3a6
Solymosi Tamás <96898224+EgyipTomiCreations@users.noreply.github.com>
2025-02-20 21:13:47 +0100

    Migration (#28)
    * update: migration
    
    * added ability to specify domain, port and security using env var
    
    ---------
    
    Co-authored-by: Milan <fulopmilan170@gmail.com>

--- src/lib/global/global.ts
+++ src/lib/global/global.ts
@@ -1,5 +1,7 @@
-const domain = "localhost"
-const secure = false;
-const port = 8080
+require('dotenv').config();
+
+const domain = process.env.NEXT_PUBLIC_DOMAIN;
+const secure = process.env.NEXT_PUBLIC_SECURE === 'true';
+const port = process.env.NEXT_PUBLIC_PORT;
 
 export {domain, secure, port}
