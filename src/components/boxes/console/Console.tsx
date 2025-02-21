2f0215787e3830a6a6491324870005b49a9cb337
Milan <fulopmilan170@gmail.com>
2025-02-21 22:19:48 +0100

    fix: console block overflow (#36)

--- src/components/boxes/console/Console.tsx
+++ src/components/boxes/console/Console.tsx
@@ -4,16 +4,16 @@ interface ConsoleDisplayProps {
   title: string;
   text: string;
 }
-  
+
 export default function ConsoleDisplay({ text, title }: ConsoleDisplayProps) {
-    if (!text) return null;
-    
-    return (
-      <div className={styles.consoleContainer}>
-        <h3 className="font-semibold">{title}</h3>
-        <pre className="bg-gray-900 text-green-400 p-4 rounded-lg">
-            {text}
-        </pre>
-      </div>
-    );
-  }
+  if (!text) return null;
+  
+  return (
+    <div className={styles.consoleContainer}>
+      <h3 className={styles.title}>{title}</h3>
+      <pre className="text-green-400 p-4 rounded-lg">
+        {text}
+      </pre>
+    </div>
+  );
+}
